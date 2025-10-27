e77b43b8880152a40372bbb04b327098db2e6761
ReKorTiS <rekortis@mail.ru>
2025-10-27 20:57:05 +0000

    add logout

--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,18 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::get('logout', [UserController::class, 'logout']);
+    Route::get('users', [UserController::class, 'index']);
+});
 
-Route::get('/users', [UserController::class, 'index']);
+Route::post('register', [UserController::class, 'register']);
 
-Route::post('/register', [UserController::class, 'register']);
+Route::post('login', [UserController::class, 'login']);
 
-Route::post('/login', [UserController::class, 'login']);
+Route::get('guest', function () {
+    return 'no This auth';
+});
